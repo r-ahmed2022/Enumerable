f1f3353afd6995272af1b9b653a37fb61977b28a
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-03 21:52:51 +0530

    fixed linters

--- mylist.rb
+++ mylist.rb
@@ -10,9 +10,7 @@ class MyList
   end
 end
 
-
 list = MyList.new(1, 2, 3, 4)
-
 
 puts(list.all? { |e| e < 5 }) #=> true
 
@@ -23,4 +21,3 @@ puts(list.any? { |e| e == 2 }) #=> true
 puts(list.any? { |e| e == 5 }) #=> false
 
 print(list.filter(&:even?)) #=> [2, 4]
-
